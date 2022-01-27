5a3f7bbb5abfc5204a18f0f19c393cf0ea017348
IBXCODECAT <nathan.schmitt7@gmail.com>
2022-01-26 19:26:39 -0600

    ho

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -45,6 +45,10 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    frontRight.setInverted(true);
+    backRight.setInverted(true);
+    backLeft.setInverted(false);
+    frontLeft.setInverted(false);
   }
 
   /**
